b1794fcb435f8ea90efa1acf8c9fc51f1a013bf6
lilly zamara zambrano sanchez <lillyzamarazambranosanchez@MacBook-Pro-de-lilly.local>
2021-05-22 19:37:59 -0500

    playa2Â

--- app-web/src/app/carrouselc1/carrouselc1.component.ts
+++ app-web/src/app/carrouselc1/carrouselc1.component.ts
@@ -5,8 +5,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./carrouselc1.component.css']
 })
 export class Carrouselc1Component implements OnInit {
-  imagc11 : string = "assets/images/img-col1/como-llegar-g.jpg";
-  imagc12 : string = "assets/images/img-col1/qv-laplaya.jpg";
+  imagc11 : string = "assets/images/img-col1/slider-c1.jpg";
+  imagc12 : string = "assets/images/img-col1/slider-c2.jpg";
+  imagc13 : string = "assets/images/img-col1/slider-c3.jpg";
+  imagc14 : string = "assets/images/img-col1/slider-c4.jpg";
+  imagc15 : string = "assets/images/img-col1/slider-c5.jpg";
   constructor() { }
   ngOnInit(): void {
   }
